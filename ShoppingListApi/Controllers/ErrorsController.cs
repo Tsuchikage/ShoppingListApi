d55ea1d64e06c56a1dd7421717bf8002ba179b1a
Mu <77066847+Tsuchikage@users.noreply.github.com>
2023-02-16 19:58:07 +0300

    .

---
+++ ShoppingListApi/Controllers/ErrorsController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorsController : ControllerBase
+    {
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnvironment)
+        {
+            if (!hostEnvironment.IsDevelopment())
+            {
+                return NotFound();
+            }
+
+            var exeptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+            return Problem(detail: exeptionHandlerFeature.Error.StackTrace,
+                title: exeptionHandlerFeature.Error.Message);
+        }
+
+        [Route("/error")]
+        public IActionResult HandleError()
+        {
+            return Problem();
+        }
+
+        [HttpGet("Throw")]
+        public IActionResult Throw()
+        {
+            throw new Exception("Sample Exception");
+        }
+    }
+}
